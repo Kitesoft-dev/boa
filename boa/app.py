0962bed5d408751d761e44378ae4edd933bef300
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-03-11 13:04:04 +0100

    Implementato il backup di Boa su singolo file

--- boa/app.py
+++ boa/app.py
@@ -1,40 +1,24 @@
 import typing
-from .adapters import BaseAdapter, get_adapter
-from .core import BackupStatus
-from pathlib import Path
+import exceptions
+import adapters
+import core
 
 
 class Boa:
     """Boa is the main entry for the application"""
 
-    def __init__(self, adapter: typing.Union[str, BaseAdapter]):
+    def __init__(self, adapter: typing.Union[str, adapters.BaseAdapter]):
         if isinstance(adapter, str):
-            adapter = get_adapter(adapter)
+            adapter = adapters.get_adapter(adapter)
         self.adapter = adapter
 
-    def backup(self, file: typing.Union[str, Path]) -> BackupStatus:
+    def backup(self, backuppable: core.Backuppable, dst: core.Destination) -> core.Status:
         """Backup the selected file
 
-        Parameters
-        ----------
-        file:
-            The file to backup; can be either a path-like object, or a bytestream
-        Returns
-        -------
-        BackupStatus
-            the status of backup
+        :param backuppable: The file to backup or the command to run
+        :param dst: The destination of backup
+        :return: Status code of backup
         """
-        if self._type_checker(file, [str, Path]):
-            return self._backup_fp(file)
-        elif self._type_checker(file, []):
-            pass
-
-    def _backup_fp(self, filepath: typing.Union[str, Path]) -> BackupStatus:
-
-        return BackupStatus.OK
-
-    @staticmethod
-    def _type_checker(obj: typing.Any, types: typing.Iterable) -> bool:
-        if not types:
-            raise ValueError("types can't be empty")
-        return any(isinstance(obj, t) for t in types)
+        raw = bytes(backuppable)
+        status = self.adapter.backup(raw, dst)
+        return status
