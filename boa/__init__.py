5980439d35aaba2f203cc9b78e9fdb1c0cd51322
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-03-14 16:16:37 +0100

    Add credits

--- boa/__init__.py
+++ boa/__init__.py
@@ -2,4 +2,7 @@ from . import adapters
 from .app import Boa
 
 __all__ = ["adapters", "Boa"]
+
 __version__ = "0.0.1"
+__author__ = "Cristiano Salerno"
+__author_email__ = "cristianosalerno1@gmail.com"
