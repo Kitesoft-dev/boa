dec7a9b7f3bcde8e05f78098d96f52dcb0c484c3
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-03-18 14:33:01 +0100

    Fix files for packaging and upload

--- boa/__init__.py
+++ boa/__init__.py
@@ -3,6 +3,4 @@ from .app import Boa, backup
 
 __all__ = ["adapters", "Boa", "backup"]
 
-__version__ = "0.0.1"
-__author__ = "Cristiano Salerno"
-__author_email__ = "cristianosalerno1@gmail.com"
+__version__ = "0.1"
