e0840f7f3ccfd87450d3d4066a27162e72077b3d
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-03-22 21:50:20 +0100

    Try passenv inline

--- tests/unit/test_ext_telegram.py
+++ tests/unit/test_ext_telegram.py
@@ -12,16 +12,6 @@ CHAT_IDS = "TELEGRAM_CHAT_IDS"
 dotenv.load_dotenv()
 
 
-def test_send_ok():
-    # do the actual backup
-    src = core.FilePathSource("setup.py")
-    dst = TelegramBotDestination(
-        filename="setup.py",
-        message="Sample message, even emojis are supported! ❤😁👍🙌😎🐱‍🚀✔👀",
-    )
-    dst.write(bytes(src))
-
-
 def test_missing_token():
     with set_env({TOKEN: ""}):
         with pytest.raises(ValueError):
@@ -38,3 +28,13 @@ def test_invalid_chat():
     with set_env({CHAT_IDS: ""}):
         with pytest.raises(ValueError):
             TelegramBotDestination()
+
+
+def test_send_ok():
+    # do the actual backup
+    src = core.FilePathSource("setup.py")
+    dst = TelegramBotDestination(
+        filename="setup.py",
+        message="Sample message, even emojis are supported! ❤😁👍🙌😎🐱‍🚀✔👀",
+    )
+    dst.write(bytes(src))
